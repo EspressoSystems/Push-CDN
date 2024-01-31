9586a2d8d2c2dbb88d142fdb2454d7071ee89b58
Rob <rob@espressosys.com>
2024-01-31 13:57:46 -0500

    lint

--- proto/src/connection/sticky.rs
+++ proto/src/connection/sticky.rs
@@ -7,14 +7,14 @@ use jf_primitives::signatures::SignatureScheme as JfSignatureScheme;
 
 use super::Connection;
 
-/// `Sticky` is a wrapper around a `Fallible` connection.
-///
-/// It employs synchronization around a `Fallible`, as well as retry logic for both switching
-/// connections at a certain failure threshold and reconnecting under said threshold.
-///
-/// Can be cloned to provide a handle to the same underlying elastic connection.
-#[derive(Clone)]
-pub struct Sticky<SignatureScheme: JfSignatureScheme, Protocol: Connection> {
-    inner: Arc<StickyInner<SignatureScheme>>,
-}
+// /// `Sticky` is a wrapper around a `Fallible` connection.
+// ///
+// /// It employs synchronization around a `Fallible`, as well as retry logic for both switching
+// /// connections at a certain failure threshold and reconnecting under said threshold.
+// ///
+// /// Can be cloned to provide a handle to the same underlying elastic connection.
+// #[derive(Clone)]
+// pub struct Sticky<SignatureScheme: JfSignatureScheme, Protocol: Connection> {
+//     inner: Arc<StickyInner<SignatureScheme>>,
+// }
 
