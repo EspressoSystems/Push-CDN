45ebc66e1c11f494706e1d2f54a03a6110172c16
Rob <rob@espressosys.com>
2025-04-15 13:05:49 -0400

    add IP address to connection struct

--- cdn-proto/src/connection/protocols/tcp_tls.rs
+++ cdn-proto/src/connection/protocols/tcp_tls.rs
@@ -129,7 +129,7 @@ impl Protocol for TcpTls {
         let (receiver, sender) = tokio::io::split(stream);
 
         // Convert the streams into a `Connection`
-        let connection = Connection::from_streams(sender, receiver, limiter);
+        let connection = Connection::from_streams(sender, receiver, limiter, remote_endpoint.ip());
 
         Ok(connection)
     }
@@ -189,6 +189,14 @@ impl UnfinalizedConnection for UnfinalizedTcpTlsConnection {
     /// # Errors
     /// Does not actually error, but satisfies trait bounds.
     async fn finalize(self, limiter: Limiter) -> Result<Connection> {
+        // Get the IP of the remote peer before we finalize the connection
+        let remote_addr = bail!(
+            self.tcp_stream.peer_addr(),
+            Connection,
+            "failed to get remote address"
+        )
+        .ip();
+
         // Wrap the stream in the TLS connection
         let stream = bail!(
             bail!(
@@ -208,7 +216,7 @@ impl UnfinalizedConnection for UnfinalizedTcpTlsConnection {
         let (receiver, sender) = tokio::io::split(stream);
 
         // Convert the streams into a `Connection`
-        let connection = Connection::from_streams(sender, receiver, limiter);
+        let connection = Connection::from_streams(sender, receiver, limiter, remote_addr);
 
         Ok(connection)
     }
